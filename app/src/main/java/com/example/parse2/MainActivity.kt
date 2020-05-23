547191c4bfdda2bf12befcaba09e1c6a6e31590b
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-05-23 13:02:19 +0900

    1

--- app/src/main/java/com/example/parse2/MainActivity.kt
+++ app/src/main/java/com/example/parse2/MainActivity.kt
@@ -51,9 +51,3 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-class HomeFeed(val videos: List<Video>)
-
-class Video(val id: Int, val name: String, val link: String, val imageUrl: String,
-            val memberOfViews: Int, val channel: Channel)
-
-class Channel(val name: String, val fileImageUrl: String)
--- app/src/main/java/com/example/parse2/MainAdapter.kt
+++ app/src/main/java/com/example/parse2/MainAdapter.kt
@@ -4,6 +4,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import coil.api.load
+import coil.transform.CircleCropTransformation
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.video_row.view.*
 
 class MainAdapter(val homeFeed: HomeFeed) : RecyclerView.Adapter<CustomViewHolder>() {
@@ -26,6 +29,17 @@ class MainAdapter(val homeFeed: HomeFeed) : RecyclerView.Adapter<CustomViewHolde
         //val videoTitle = videoTitle[position]
         val video = homeFeed.videos[position]
         holder.view.textView_video_title.text = video.name
+
+        holder.view.textView_channel_name.text = video.channel.name + " * " + "20K Views\n4 days ago "
+
+        val thumbnailImageView = holder.view.imageView_video_thumbnail
+        thumbnailImageView.load(video.imageUrl)
+
+        val channelProfileImageView = holder.view.imageView_channel_profile
+        channelProfileImageView.load(video.channel.profileImageUrl) {
+            crossfade(true)
+            transformations(CircleCropTransformation())
+        }
     }
 }
 
